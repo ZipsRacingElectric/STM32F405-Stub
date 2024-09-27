e6bfd5f4e06a00ed661f570672e8a22fba37f39e
Cole Barach <colebarach@tutanota.com>
2024-09-26 20:35:32 -0400

    Added the STM34F405-Common toolchain as a submodule,

--- common/board.mk
+++
@@ -1,9 +0,0 @@
-BOARD_CONF := $(CONFDIR)/board.chcfg
-BOARD_FILES := $(BOARDDIR)/board.h $(BOARDDIR)/board.c
-
-$(BOARD_FILES) &: $(BOARD_CONF)
-	mkdir -p $(BOARDDIR)
-	fmpp --data-root=$(CONFDIR) -S $(CHIBIOS_SOURCE_PATH)/tools/ftl/processors/boards/stm32f4xx/templates		\
-		--freemarker-links=lib:$(CHIBIOS_SOURCE_PATH)/tools/ftl/libs -O $(BOARDDIR) -D "doc1:xml(board.chcfg)"	\
-
-board-files: $(BOARD_FILES)
--- common/openocd.mk
+++
@@ -1,2 +0,0 @@
-flash: build/$(PROJECT).elf
-	openocd -f interface/stlink.cfg -f target/stm32f4x.cfg -c "program build/$(PROJECT).elf verify reset exit"
